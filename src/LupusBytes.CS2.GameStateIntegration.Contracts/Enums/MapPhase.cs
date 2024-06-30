92c8dacbedb2dd7e15db29da804746818c1a3f49
lupusbytes <bertram@lupusbytes.com>
2024-07-01 01:10:12 +0400

    docs: add xml summary to map phases

--- src/LupusBytes.CS2.GameStateIntegration.Contracts/Enums/MapPhase.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Contracts/Enums/MapPhase.cs
@@ -5,7 +5,23 @@ namespace LupusBytes.CS2.GameStateIntegration.Contracts.Enums;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum MapPhase
 {
-    Warmup,
+    /// <summary>
+    /// The game is in freeze time.
+    /// </summary>
+    Freezetime,
+
+    /// <summary>
+    /// The round or game is undergoing.
+    /// </summary>
     Live,
+
+    /// <summary>
+    /// The game is in warmup.
+    /// </summary>
+    Warmup,
+
+    /// <summary>
+    /// The game is in intermission.
+    /// </summary>
     Intermission,
 }
