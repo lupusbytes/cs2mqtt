f1658c76151a68bd727b446f5962c8bfc0f48fc6
lupusbytes <bertram@lupusbytes.com>
2024-07-13 03:10:52 +0400

    chore: add json attribute

--- src/LupusBytes.CS2.GameStateIntegration.Contracts/TeamMapDetails.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Contracts/TeamMapDetails.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 namespace LupusBytes.CS2.GameStateIntegration.Contracts;
 
 public record TeamMapDetails(
-    int Score,
+    [property: JsonPropertyName("score")] int Score,
     [property: JsonPropertyName("consecutive_round_losses")] int ConsecutiveRoundLosses,
     [property: JsonPropertyName("timeouts_remaining")] int TimeoutsRemaining,
     [property: JsonPropertyName("matches_won_this_series")] int MatchesWonThisSeries);
