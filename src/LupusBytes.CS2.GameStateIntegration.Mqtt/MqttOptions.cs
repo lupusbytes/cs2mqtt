abb0c319ab9171c3a2d5c343317e59c4b7be61d0
lupusbytes <bertram@lupusbytes.com>
2024-07-13 02:51:59 +0400

    docs: add xml documentation to mqtt options class

--- src/LupusBytes.CS2.GameStateIntegration.Mqtt/MqttOptions.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt/MqttOptions.cs
@@ -2,11 +2,23 @@ namespace LupusBytes.CS2.GameStateIntegration.Mqtt;
 
 public class MqttOptions
 {
+    /// <summary>
+    /// Key for getting the <see cref="MqttOptions"/> subsection from the configuration.
+    /// </summary>
     public const string Section = "Mqtt";
 
+    /// <summary>
+    /// The hostname or IP address of the MQTT broker.
+    /// </summary>
     public string Host { get; set; } = "localhost";
 
+    /// <summary>
+    /// The port number to use when connecting to the MQTT broker.
+    /// </summary>
     public int Port { get; set; } = 1883;
 
+    /// <summary>
+    /// Whether to use SSL/TLS when connecting to the MQTT broker. The default setting is <see langword="false" />.
+    /// </summary>
     public bool UseTls { get; set; }
 }
