52cb88bdf99c00e08890720bc467fbadfa1fceea
lupusbytes <bertram@lupusbytes.com>
2024-07-13 03:08:17 +0400

    feat: map matches_won_this_series

--- src/LupusBytes.CS2.GameStateIntegration.Contracts/TeamMapDetails.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Contracts/TeamMapDetails.cs
@@ -2,9 +2,8 @@ using System.Text.Json.Serialization;
 
 namespace LupusBytes.CS2.GameStateIntegration.Contracts;
 
-// Omitted:
-// "matches_won_this_series": 0
 public record TeamMapDetails(
     int Score,
     [property: JsonPropertyName("consecutive_round_losses")] int ConsecutiveRoundLosses,
-    [property: JsonPropertyName("timeouts_remaining")] int TimeoutsRemaining);
+    [property: JsonPropertyName("timeouts_remaining")] int TimeoutsRemaining,
+    [property: JsonPropertyName("matches_won_this_series")] int MatchesWonThisSeries);
