7cee62d26e40212ea731825d4ad6f1053502a583
lupusbytes <bertram@lupusbytes.com>
2024-07-12 18:14:17 +0400

    chore(contracts): remove tostring overrides

--- src/LupusBytes.CS2.GameStateIntegration.Contracts/Map.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Contracts/Map.cs
@@ -9,13 +9,4 @@ public record Map(
     [property: JsonPropertyName("phase")] MapPhase Phase,
     [property: JsonPropertyName("round")] int Round,
     [property: JsonPropertyName("team_t")] TeamMapDetails T,
-    [property: JsonPropertyName("team_ct")] TeamMapDetails CT)
-{
-    public override string ToString() =>
-        $"Map:      {Name}{Environment.NewLine}" +
-        $"Mode:     {Mode}{Environment.NewLine}" +
-        $"Phase:    {Phase}{Environment.NewLine}" +
-        $"Round:    {Round}{Environment.NewLine}" +
-        $"T Score:  {T.Score}{Environment.NewLine}" +
-        $"CT Score: {CT.Score}{Environment.NewLine}";
-}
+    [property: JsonPropertyName("team_ct")] TeamMapDetails CT);
--- src/LupusBytes.CS2.GameStateIntegration.Contracts/Player.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Contracts/Player.cs
@@ -11,23 +11,4 @@ public record Player(
     [property: JsonPropertyName("name")] string Name,
     [property: JsonPropertyName("team")] Team? Team,
     [property: JsonPropertyName("activity")] Activity Activity,
-    [property: JsonPropertyName("state")] PlayerState? State)
-{
-    public override string ToString()
-    {
-        var output = $"SteamID64: {SteamId64}{Environment.NewLine}" +
-                     $"Name: {Name}{Environment.NewLine}" +
-                     $"Team: {Team}{Environment.NewLine}" +
-                     $"Activity: {Activity}{Environment.NewLine}";
-
-        if (State is not null)
-        {
-            output += $"Health: {State.Health}{Environment.NewLine}" +
-                      $"Flashed: {State.Flashed}{Environment.NewLine}" +
-                      $"Smoked: {State.Smoked}{Environment.NewLine}" +
-                      $"Kills in round: {State.RoundKills}{Environment.NewLine}";
-        }
-
-        return output;
-    }
-}
+    [property: JsonPropertyName("state")] PlayerState? State);
