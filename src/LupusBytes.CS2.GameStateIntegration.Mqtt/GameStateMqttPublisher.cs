f2eb84c5760e91a3c87335ad0725b3b5a170fa77
lupusbytes <bertram@lupusbytes.com>
2024-07-20 17:54:15 +0400

    feat: parallelize processing for each type of event

--- src/LupusBytes.CS2.GameStateIntegration.Mqtt/GameStateMqttPublisher.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt/GameStateMqttPublisher.cs
@@ -15,17 +15,30 @@ public sealed class GameStateMqttPublisher(
 {
     public const string BaseTopic = "cs2mqtt";
 
-    private readonly Channel<BaseEvent> channel = Channel.CreateBounded<BaseEvent>(new BoundedChannelOptions(1000)
+    private static readonly BoundedChannelOptions ChannelOptions = new(1000)
     {
         SingleWriter = false,
         SingleReader = true,
         FullMode = BoundedChannelFullMode.DropOldest,
-    });
+    };
 
-    public void OnNext(MapEvent value) => channel.Writer.TryWrite(value);
-    public void OnNext(RoundEvent value) => channel.Writer.TryWrite(value);
-    public void OnNext(PlayerEvent value) => channel.Writer.TryWrite(value);
-    public void OnNext(PlayerStateEvent value) => channel.Writer.TryWrite(value);
+    private readonly Channel<PlayerEvent> playerChannel = Channel.CreateBounded<PlayerEvent>(ChannelOptions);
+    private readonly Channel<PlayerStateEvent> playerStateChannel = Channel.CreateBounded<PlayerStateEvent>(ChannelOptions);
+    private readonly Channel<MapEvent> mapChannel = Channel.CreateBounded<MapEvent>(ChannelOptions);
+    private readonly Channel<RoundEvent> roundChannel = Channel.CreateBounded<RoundEvent>(ChannelOptions);
+
+    public void OnNext(PlayerEvent value) => playerChannel.Writer.TryWrite(value);
+    public void OnNext(PlayerStateEvent value) => playerStateChannel.Writer.TryWrite(value);
+    public void OnNext(MapEvent value) => mapChannel.Writer.TryWrite(value);
+    public void OnNext(RoundEvent value) => roundChannel.Writer.TryWrite(value);
+
+    public void OnCompleted()
+    {
+    }
+
+    public void OnError(Exception error)
+    {
+    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -34,20 +47,26 @@ public sealed class GameStateMqttPublisher(
         using var roundSubscription = gameStateService.Subscribe(this as IObserver<RoundEvent>);
         using var mapSubscription = gameStateService.Subscribe(this as IObserver<MapEvent>);
 
-        while (await channel.Reader.WaitToReadAsync(stoppingToken))
+        var tasks = new[]
+        {
+            ProcessChannelAsync(playerChannel, stoppingToken),
+            ProcessChannelAsync(playerStateChannel, stoppingToken),
+            ProcessChannelAsync(mapChannel, stoppingToken),
+            ProcessChannelAsync(roundChannel, stoppingToken),
+        };
+
+        await Task.WhenAll(tasks);
+    }
+
+    private async Task ProcessChannelAsync<TEvent>(Channel<TEvent> channel, CancellationToken cancellationToken)
+        where TEvent : BaseEvent
+    {
+        while (await channel.Reader.WaitToReadAsync(cancellationToken))
         {
             while (channel.Reader.TryRead(out var @event))
             {
-                await mqttClient.PublishAsync(@event.ToMqttMessage(), stoppingToken);
+                await mqttClient.PublishAsync(@event.ToMqttMessage(), cancellationToken);
             }
         }
-    }
-
-    public void OnCompleted()
-    {
-    }
-
-    public void OnError(Exception error)
-    {
     }
 }
