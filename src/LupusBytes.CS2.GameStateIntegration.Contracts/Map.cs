9e43aec3e12fc66ea33a579d551799ae9575f692
lupusbytes <bertram@lupusbytes.com>
2024-07-20 15:51:49 +0400

    docs: add xml summary for map contract

--- src/LupusBytes.CS2.GameStateIntegration.Contracts/Map.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Contracts/Map.cs
@@ -3,6 +3,14 @@ using LupusBytes.CS2.GameStateIntegration.Contracts.Enums;
 
 namespace LupusBytes.CS2.GameStateIntegration.Contracts;
 
+/// <summary>
+/// Information about the Map.
+/// Enabled by including <code>"map": "1"</code> in the game state cfg file.
+/// </summary>
+/// <param name="Mode">The game mode.</param>
+/// <param name="Name">The map name.</param>
+/// <param name="Phase">The current map phase.</param>
+/// <param name="Round">The current round number.</param>
 public record Map(
     [property: JsonPropertyName("mode")] Mode? Mode,
     [property: JsonPropertyName("name")] string Name,
