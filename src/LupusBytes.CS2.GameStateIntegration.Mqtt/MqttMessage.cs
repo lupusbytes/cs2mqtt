ed0b77dde252aa7925295e6b777668934fc416ae
lupusbytes <bertram@lupusbytes.com>
2024-07-20 17:30:20 +0400

    perf: convert mqtt message class to record struct

--- src/LupusBytes.CS2.GameStateIntegration.Mqtt/MqttMessage.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt/MqttMessage.cs
@@ -1,8 +1,3 @@
 namespace LupusBytes.CS2.GameStateIntegration.Mqtt;
 
-public class MqttMessage
-{
-    public string Topic { get; set; } = string.Empty;
-
-    public string Payload { get; set; } = string.Empty;
-}
+public record struct MqttMessage(string Topic, string Payload);
