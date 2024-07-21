04bb56c0d2fe1d063ff32cc7bcd22ff499db9af9
lupusbytes <bertram@lupusbytes.com>
2024-07-22 01:54:15 +0400

    test: add test for service provider registering of game state service

---
+++ test/LupusBytes.CS2.GameStateIntegration.Tests/Extensions/ServiceCollectionExtensionsTest.cs
@@ -0,0 +1,22 @@
+using LupusBytes.CS2.GameStateIntegration.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LupusBytes.CS2.GameStateIntegration.Tests.Extensions;
+
+public class ServiceCollectionExtensionsTest
+{
+    [Theory, AutoNSubstituteData]
+    public void AddGameStateService_registers_GameStateService(IConfiguration configuration)
+    {
+        // Arrange
+        var sut = new ServiceCollection();
+        sut.AddGameStateService(configuration);
+
+        // Act & Assert
+        sut.BuildServiceProvider()
+            .GetRequiredService<IGameStateService>()
+            .Should()
+            .BeOfType<GameStateService>();
+    }
+}
