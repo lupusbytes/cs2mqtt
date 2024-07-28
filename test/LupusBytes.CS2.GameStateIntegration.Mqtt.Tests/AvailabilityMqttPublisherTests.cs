1d8de95fbccdf7a9d8671a43ecdd1fe484215819
lupusbytes <bertram@lupusbytes.com>
2024-07-29 00:59:16 +0400

    test: assert player availability is set to online when receiving event

--- test/LupusBytes.CS2.GameStateIntegration.Mqtt.Tests/AvailabilityMqttPublisherTests.cs
+++ test/LupusBytes.CS2.GameStateIntegration.Mqtt.Tests/AvailabilityMqttPublisherTests.cs
@@ -1,3 +1,6 @@
+using LupusBytes.CS2.GameStateIntegration.Contracts;
+using LupusBytes.CS2.GameStateIntegration.Events;
+
 namespace LupusBytes.CS2.GameStateIntegration.Mqtt.Tests;
 
 public class AvailabilityMqttPublisherTests
@@ -25,6 +28,38 @@ public class AvailabilityMqttPublisherTests
         await mqttClient.Received(1).PublishAsync(
             Arg.Is<MqttMessage>(x =>
                 x.Topic == MqttConstants.SystemAvailabilityTopic &&
+                x.Payload == "online" &&
+                x.RetainFlag),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Theory, AutoNSubstituteData]
+    public async Task Publishes_player_availability(
+        [Frozen] IMqttClient mqttClient,
+        SteamId64 steamId,
+        Player player,
+        AvailabilityMqttPublisher sut)
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        var tcs = new TaskCompletionSource<bool>();
+        cts.Token.Register(() => tcs.TrySetCanceled(CancellationToken.None));
+        mqttClient
+            .When(x => x.PublishAsync(
+                Arg.Is<MqttMessage>(m => m.Topic == $"{MqttConstants.BaseTopic}/{steamId}/player/status"),
+                Arg.Any<CancellationToken>()))
+            .Do(_ => tcs.SetResult(true));
+
+        await sut.StartAsync(CancellationToken.None);
+
+        // Act
+        sut.OnNext(new PlayerEvent(steamId, player));
+
+        // Assert
+        await tcs.Task;
+        await mqttClient.Received(1).PublishAsync(
+            Arg.Is<MqttMessage>(x =>
+                x.Topic == $"{MqttConstants.BaseTopic}/{steamId}/player/status" &&
                 x.Payload == "online" &&
                 x.RetainFlag),
             Arg.Any<CancellationToken>());
