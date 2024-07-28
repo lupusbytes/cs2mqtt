21d684befeca730b6a0f96c05f4476e4c0bc3e68
lupusbytes <bertram@lupusbytes.com>
2024-07-29 01:22:37 +0400

    test: assert that availability become online and offline based on events

--- test/LupusBytes.CS2.GameStateIntegration.Mqtt.Tests/AvailabilityMqttPublisherTests.cs
+++ test/LupusBytes.CS2.GameStateIntegration.Mqtt.Tests/AvailabilityMqttPublisherTests.cs
@@ -160,4 +160,59 @@ public class AvailabilityMqttPublisherTests
                 x.RetainFlag),
             Arg.Any<CancellationToken>());
     }
+
+    [Theory, AutoNSubstituteData]
+    public async Task Publishes_and_updates_availability_on_changes(
+        [Frozen] IMqttClient mqttClient,
+        SteamId64 steamId,
+        Player player,
+        AvailabilityMqttPublisher sut)
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        var tcs = new TaskCompletionSource<bool>();
+        cts.Token.Register(() => tcs.TrySetCanceled(CancellationToken.None));
+        var receivedTimes = 0;
+        mqttClient
+            .When(x => x.PublishAsync(
+                Arg.Any<MqttMessage>(),
+                Arg.Any<CancellationToken>()))
+            .Do(_ =>
+            {
+                // We expect there to be a total of 4 MQTT messages published in this test.
+                // 1: System online
+                // 2: Player online
+                // 3: Player offline
+                // 4: Player online
+                Interlocked.Increment(ref receivedTimes);
+                if (receivedTimes == 4)
+                {
+                    tcs.SetResult(true);
+                }
+            });
+
+        await sut.StartAsync(cts.Token);
+
+        // Act
+        sut.OnNext(new PlayerEvent(steamId, player));
+        sut.OnNext(new PlayerEvent(steamId, Player: null));
+        sut.OnNext(new PlayerEvent(steamId, player));
+
+        // Assert
+        await tcs.Task;
+
+        await mqttClient.Received(2).PublishAsync(
+            Arg.Is<MqttMessage>(x =>
+                x.Topic == $"{MqttConstants.BaseTopic}/{steamId}/player/status" &&
+                x.Payload == "online" &&
+                x.RetainFlag),
+            Arg.Any<CancellationToken>());
+
+        await mqttClient.Received(1).PublishAsync(
+            Arg.Is<MqttMessage>(x =>
+                x.Topic == $"{MqttConstants.BaseTopic}/{steamId}/player/status" &&
+                x.Payload == "offline" &&
+                x.RetainFlag),
+            Arg.Any<CancellationToken>());
+    }
 }
