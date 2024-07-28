98bfea942483a3d19ec7bdb4f695d6ee0cc15fdf
lupusbytes <bertram@lupusbytes.com>
2024-07-29 00:59:31 +0400

    test: assert player-state availability is set to online when receiving event

--- test/LupusBytes.CS2.GameStateIntegration.Mqtt.Tests/AvailabilityMqttPublisherTests.cs
+++ test/LupusBytes.CS2.GameStateIntegration.Mqtt.Tests/AvailabilityMqttPublisherTests.cs
@@ -64,4 +64,36 @@ public class AvailabilityMqttPublisherTests
                 x.RetainFlag),
             Arg.Any<CancellationToken>());
     }
+
+    [Theory, AutoNSubstituteData]
+    public async Task Publishes_player_state_availability(
+        [Frozen] IMqttClient mqttClient,
+        SteamId64 steamId,
+        PlayerState playerState,
+        AvailabilityMqttPublisher sut)
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        var tcs = new TaskCompletionSource<bool>();
+        cts.Token.Register(() => tcs.TrySetCanceled(CancellationToken.None));
+        mqttClient
+            .When(x => x.PublishAsync(
+                Arg.Is<MqttMessage>(m => m.Topic == $"{MqttConstants.BaseTopic}/{steamId}/player-state/status"),
+                Arg.Any<CancellationToken>()))
+            .Do(_ => tcs.SetResult(true));
+
+        await sut.StartAsync(CancellationToken.None);
+
+        // Act
+        sut.OnNext(new PlayerStateEvent(steamId, playerState));
+
+        // Assert
+        await tcs.Task;
+        await mqttClient.Received(1).PublishAsync(
+            Arg.Is<MqttMessage>(x =>
+                x.Topic == $"{MqttConstants.BaseTopic}/{steamId}/player-state/status" &&
+                x.Payload == "online" &&
+                x.RetainFlag),
+            Arg.Any<CancellationToken>());
+    }
 }
