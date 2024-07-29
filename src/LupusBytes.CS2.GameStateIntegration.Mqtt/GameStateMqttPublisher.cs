bbe1ba486d57e739101dc66d87f4ab385f3b9884
lupusbytes <bertram@lupusbytes.com>
2024-07-29 23:54:20 +0400

    feat: use base class for mqtt publisher background services

--- src/LupusBytes.CS2.GameStateIntegration.Mqtt/GameStateMqttPublisher.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt/GameStateMqttPublisher.cs
@@ -1,68 +1,32 @@
 using System.Threading.Channels;
 using LupusBytes.CS2.GameStateIntegration.Events;
 using LupusBytes.CS2.GameStateIntegration.Mqtt.Extensions;
-using Microsoft.Extensions.Hosting;
 
 namespace LupusBytes.CS2.GameStateIntegration.Mqtt;
 
 public sealed class GameStateMqttPublisher(
     IGameStateService gameStateService,
-    IMqttClient mqttClient) : BackgroundService,
-    IObserver<MapEvent>,
-    IObserver<PlayerEvent>,
-    IObserver<PlayerStateEvent>,
-    IObserver<RoundEvent>
+    IMqttClient mqttClient) : GameStateObserverService(gameStateService)
 {
-    private static readonly BoundedChannelOptions ChannelOptions = new(1000)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        SingleWriter = false,
-        SingleReader = true,
-        FullMode = BoundedChannelFullMode.DropOldest,
-    };
-
-    private readonly Channel<PlayerEvent> playerChannel = Channel.CreateBounded<PlayerEvent>(ChannelOptions);
-    private readonly Channel<PlayerStateEvent> playerStateChannel = Channel.CreateBounded<PlayerStateEvent>(ChannelOptions);
-    private readonly Channel<MapEvent> mapChannel = Channel.CreateBounded<MapEvent>(ChannelOptions);
-    private readonly Channel<RoundEvent> roundChannel = Channel.CreateBounded<RoundEvent>(ChannelOptions);
-
-    public void OnNext(PlayerEvent value) => playerChannel.Writer.TryWrite(value);
-    public void OnNext(PlayerStateEvent value) => playerStateChannel.Writer.TryWrite(value);
-    public void OnNext(MapEvent value) => mapChannel.Writer.TryWrite(value);
-    public void OnNext(RoundEvent value) => roundChannel.Writer.TryWrite(value);
-
-    public void OnCompleted()
-    {
-    }
-
-    public void OnError(Exception error)
-    {
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        using var playerSubscription = gameStateService.Subscribe(this as IObserver<PlayerEvent>);
-        using var playerStateSubscription = gameStateService.Subscribe(this as IObserver<PlayerStateEvent>);
-        using var roundSubscription = gameStateService.Subscribe(this as IObserver<RoundEvent>);
-        using var mapSubscription = gameStateService.Subscribe(this as IObserver<MapEvent>);
-
         var tasks = new[]
         {
-            ProcessChannelAsync(playerChannel, stoppingToken),
-            ProcessChannelAsync(playerStateChannel, stoppingToken),
-            ProcessChannelAsync(mapChannel, stoppingToken),
-            ProcessChannelAsync(roundChannel, stoppingToken),
+            ProcessChannelAsync(PlayerChannelReader, stoppingToken),
+            ProcessChannelAsync(PlayerStateChannelReader, stoppingToken),
+            ProcessChannelAsync(MapChannelReader, stoppingToken),
+            ProcessChannelAsync(RoundChannelReader, stoppingToken),
         };
 
-        // This task must be awaited to prevent the subscriptions from being disposed.
-        await Task.WhenAll(tasks);
+        return Task.WhenAll(tasks);
     }
 
-    private async Task ProcessChannelAsync<TEvent>(Channel<TEvent> channel, CancellationToken cancellationToken)
+    private async Task ProcessChannelAsync<TEvent>(ChannelReader<TEvent> channelReader, CancellationToken cancellationToken)
         where TEvent : BaseEvent
     {
-        while (await channel.Reader.WaitToReadAsync(cancellationToken))
+        while (await channelReader.WaitToReadAsync(cancellationToken))
         {
-            while (channel.Reader.TryRead(out var @event))
+            while (channelReader.TryRead(out var @event))
             {
                 await mqttClient.PublishAsync(@event.ToMqttMessage(), cancellationToken);
             }
