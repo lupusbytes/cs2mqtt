a8813ac337a906be20b419f7ae873bc65d585b0b
lupusbytes <bertram@lupusbytes.com>
2024-07-31 01:45:58 +0400

    feat: connect to mqtt broker with resilience and log information

--- test/LupusBytes.CS2.GameStateIntegration.Api.EndToEnd.Tests/Helpers/TestWebApplicationFactory.cs
+++ test/LupusBytes.CS2.GameStateIntegration.Api.EndToEnd.Tests/Helpers/TestWebApplicationFactory.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using LupusBytes.CS2.GameStateIntegration.Api.EndToEnd.Tests.Fakes;
 using LupusBytes.CS2.GameStateIntegration.Mqtt;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -18,12 +19,26 @@ public class TestWebApplicationFactory<TProgram> : WebApplicationFactory<TProgra
             config.AddJsonFile("appsettings.EndToEnd.json", optional: false, reloadOnChange: true);
         });
 
-        builder.ConfigureServices(services =>
-        {
-            services.RemoveAll<IMqttClient>();
-            services.AddSingleton<IMqttClient>(new FakeMqttClient());
-        });
+        builder.ConfigureServices(ReplaceMqttClientWithFake);
 
         return base.CreateHost(builder);
+    }
+
+    private static void ReplaceMqttClientWithFake(IServiceCollection services)
+    {
+        // Stop MqttClient from being registered as a HostedService
+        var descriptor = services.FirstOrDefault(descriptor =>
+            descriptor.ServiceType == typeof(IHostedService) &&
+            descriptor.ImplementationFactory?.GetMethodInfo().ReturnType == typeof(MqttClient));
+
+        if (descriptor is not null)
+        {
+            services.Remove(descriptor);
+        }
+
+        // Replace MqttClient registrations with our fake.
+        services.RemoveAll<MqttClient>();
+        services.RemoveAll<IMqttClient>();
+        services.AddSingleton<IMqttClient>(new FakeMqttClient());
     }
 }
