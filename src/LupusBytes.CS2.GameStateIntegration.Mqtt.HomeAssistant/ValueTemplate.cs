e204c46eec8cb4f234f33a838f4d92e443e2be89
lupusbytes <bertram@lupusbytes.com>
2024-10-14 00:40:45 +0400

    fix(homeassistant): show "inactive" for bomb when value is null instead of "unknown"
    When a value is null, home assistant will interpret the state as "Unknown", which looks odd. 
    The bomb is always null when it's not planted, defused or exploded.
    To fix this, we are going to create a value template that shows Inactive, instead of Unknown, which is a slightly better description.

--- src/LupusBytes.CS2.GameStateIntegration.Mqtt.HomeAssistant/ValueTemplate.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt.HomeAssistant/ValueTemplate.cs
@@ -5,6 +5,15 @@ public static class ValueTemplate
     public static string JsonPropertyValue(string propertyName)
         => $"{{{{ value_json.{propertyName} }}}}";
 
+    public static string JsonPropertyValueWithFallback(string property, string fallback) =>
+        $$$"""
+           {% if value_json.{{{property}}} is not none %}
+             {{ value_json.{{{property}}} }}
+           {% else %}
+             {{{fallback}}}
+           {% endif %}
+           """;
+
     public static string NestedJsonPropertyValue(string propertyName, string nestedPropertyName)
         => $"{{{{ value_json[\"{propertyName}\"].{nestedPropertyName} }}}}";
 }
