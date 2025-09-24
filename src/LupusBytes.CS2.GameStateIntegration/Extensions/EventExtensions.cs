34307d3c962445680deff11025e3372051293524
Bertram Baltzer Andersen <bertram@lupusbytes.com>
2025-09-24 18:47:43 +0400

    Add linebreaks between extension methods

--- src/LupusBytes.CS2.GameStateIntegration/Extensions/EventExtensions.cs
+++ src/LupusBytes.CS2.GameStateIntegration/Extensions/EventExtensions.cs
@@ -6,10 +6,15 @@ namespace LupusBytes.CS2.GameStateIntegration.Extensions;
 public static class EventExtensions
 {
     public static MapEvent ToEvent(this Map? map, SteamId64 steamId) => new(steamId, map);
+
     public static RoundEvent ToEvent(this Round? round, SteamId64 steamId) => new(steamId, round);
+
     public static PlayerEvent ToEvent(this Player? player, SteamId64 steamId) => new(steamId, player);
+
     public static PlayerStateEvent ToEvent(this PlayerState? playerState, SteamId64 steamId) => new(steamId, playerState);
+
     public static PlayerWithStateEvent ToEvent(this PlayerWithState? player, SteamId64 steamId) => new(steamId, player);
-    public static PlayerEvent ToPlayerEvent(this PlayerWithState player, SteamId64 steamId) =>
-        new Player(player.SteamId64, player.Name, player.Team, player.Activity).ToEvent(steamId);
+
+    public static PlayerEvent ToPlayerEvent(this PlayerWithState player, SteamId64 steamId)
+        => new Player(player.SteamId64, player.Name, player.Team, player.Activity).ToEvent(steamId);
 }
