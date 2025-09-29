495b387ba16c147392d8f9b3180a79a4baf7a582
davidramiro <david@kore.cc>
2025-09-29 21:48:28 +0200

    feat(wip): use hassio api to fetch mqtt info

--- src/LupusBytes.CS2.GameStateIntegration.Mqtt/Extensions/ServiceCollectionExtensions.cs
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -8,15 +10,16 @@ namespace LupusBytes.CS2.GameStateIntegration.Mqtt.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    private const string SupervisorServicesBaseAddress = "http://supervisor/";
+    private const string SupervisorServicesEndpoint = "services/mqtt";
+
     public static IServiceCollection AddMqttClient(this IServiceCollection services, IConfiguration configuration)
     {
         // Register MQTTnet.Client.IMqttClient. This instance will be injected into our own MqttClient.
         // Creating it here, instead of instantiating it inside the class, will enable us to mock/substitute it for unit tests.
         services.AddSingleton(new MqttClientFactory().CreateMqttClient());
 
-        // Get options
-        var mqttOptions = new MqttOptions();
-        configuration.GetSection(MqttOptions.Section).Bind(mqttOptions);
+        var mqttOptions = ConfigureMqttOptions(configuration);
 
         // Register concrete MqttClient class
         services.AddSingleton(sp => new MqttClient(
@@ -31,5 +34,58 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IMqttClient, MqttClient>(sp => sp.GetRequiredService<MqttClient>());
 
         return services;
+    }
+
+    private static MqttOptions ConfigureMqttOptions(IConfiguration configuration)
+    {
+        // Get options
+        var mqttOptions = new MqttOptions();
+        mqttOptions.ClientId = "cs2mqtt";
+
+        var supervisorToken = Environment.GetEnvironmentVariable("SUPERVISOR_TOKEN");
+        if (string.IsNullOrWhiteSpace(supervisorToken))
+        {
+            Console.WriteLine("No supervisor token found. Using default MQTT options.");
+            configuration.GetSection(MqttOptions.Section).Bind(mqttOptions);
+            return mqttOptions;
+        }
+
+        Console.WriteLine("Supervisor token found. Using supervisor MQTT options.");
+        var cfg = GetSupervisorMqttConfig(supervisorToken);
+        if (cfg is null)
+        {
+            configuration.GetSection(MqttOptions.Section).Bind(mqttOptions);
+            return mqttOptions;
+        }
+
+        mqttOptions.Host = cfg.Host;
+        mqttOptions.Port = cfg.Port;
+        mqttOptions.UseTls = cfg.SSL;
+
+        mqttOptions.Password = string.IsNullOrWhiteSpace(cfg.Password) ? string.Empty : cfg.Password;
+        mqttOptions.Username = string.IsNullOrWhiteSpace(cfg.Username) ? string.Empty : cfg.Username;
+        mqttOptions.ProtocolVersion = string.IsNullOrWhiteSpace(cfg.Protocol) ? string.Empty : cfg.Protocol;
+
+        // Still bind remaining settings from the config file to fill missing ones
+        configuration.GetSection(MqttOptions.Section).Bind(mqttOptions);
+
+        return mqttOptions;
+    }
+
+    private static SupervisorMqttConfig? GetSupervisorMqttConfig(string supervisorToken)
+    {
+        using var httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri(SupervisorServicesBaseAddress);
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", supervisorToken);
+
+        var response = httpClient.GetAsync(new Uri(SupervisorServicesEndpoint)).Result;
+        Console.WriteLine(response.StatusCode);
+        response.EnsureSuccessStatusCode();
+
+        var json = response.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(json);
+        var config = JsonSerializer.Deserialize<SupervisorMqttConfig>(json);
+
+        return config;
     }
 }
---
+++ src/LupusBytes.CS2.GameStateIntegration.Mqtt/Extensions/SupervisorMqttConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LupusBytes.CS2.GameStateIntegration.Mqtt.Extensions;
+
+/// <summary>
+/// Supervisor GET response for <code>/services/mqtt</code> endpoint.
+/// Contains MQTT connection info and credentials.
+/// </summary>
+/// <param name="Addon">The addon providing MQTT functionality.</param>
+/// <param name="Host">The IP of the addon running the service.</param>
+/// <param name="Port">The port the service is running on.</param>
+/// <param name="SSL">True if SSL is in use.</param>
+/// <param name="Username">The username for the service.</param>
+/// <param name="Password">The password for the service.</param>
+/// <param name="Protocol">The MQTT protocol version.</param>
+public record SupervisorMqttConfig(
+    [property: JsonPropertyName("addon")] string Addon,
+    [property: JsonPropertyName("host")] string Host,
+    [property: JsonPropertyName("port")] int Port,
+    [property: JsonPropertyName("ssl")] bool SSL,
+    [property: JsonPropertyName("Username")] string? Username,
+    [property: JsonPropertyName("Password")] string? Password,
+    [property: JsonPropertyName("Protocol")] string? Protocol);
